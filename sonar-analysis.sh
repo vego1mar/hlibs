7e0be4d8adf4d854a28d15c568a1ded7c71b2796
Paweł Siciński <vego1mar@gmail.com>
2021-07-06 19:26:24 +0200

    Configuration for manual SonarQube analysis.

---
+++ sonar-analysis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://docs.sonarqube.org/latest/analysis/scan/sonarscanner/
+# https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
+
+PARTITION_UUID="386407ee-f7b9-4feb-9332-f84151ba34f3"
+PRE_PATH="/media/$USER/$PARTITION_UUID/INSTALL"
+SCANNER_VERSION="4.6.2.2472"
+BUILD_WRAPPER="$PRE_PATH/build-wrapper-linux-x86/build-wrapper-linux-x86-64"
+SONAR_SCANNER="$PRE_PATH/sonar-scanner-$SCANNER_VERSION-linux/bin/sonar-scanner"
+BW_OUTPUT="bw_outputs"
+SONAR_BUILD="sonar-build.sh"
+
+
+$BUILD_WRAPPER --out-dir $BW_OUTPUT ./$SONAR_BUILD
+$SONAR_SCANNER
---
+++ sonar-build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+COMPILATOR=/usr/bin/gcc
+CPP_STANDARD=-std=c++2a
+FLAGS="-Wall -Wextra -Wfloat-equal -Wundef -Wshadow=global -Wpointer-arith -Wcast-align=strict -Wwrite-strings -Wcast-qual
+       -Wswitch-default -Wswitch-enum -Wconversion -Wunreachable-code -Wformat=2 -Wpedantic -Werror
+       -fexceptions -fpie -fwrapv
+       -O2
+       $CPP_STANDARD -ggdb"
+
+
+function print_build_info() {
+    local RC=$?
+
+    if [ $RC -ne 0 ]; then
+      echo "BUILD FAILURE"
+    else
+      echo "BUILD SUCCESS"
+    fi
+}
+
+function remove_previous_build() {
+    echo "REMOVE PREVIOUS BUILD"
+    rm -rf sonar-build
+}
+
+function build() {
+   echo "BUILDING"
+
+   mkdir sonar-build
+   echo "./sonar-build"
+   "$COMPILATOR" $FLAGS -c src/main.cpp -o sonar-build/main.o
+
+   echo "./sonar-build/file"
+   # mkdir sonar-build/file
+}
+
+remove_previous_build
+build
+print_build_info
